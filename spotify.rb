294d5e584c5e24df5ab62383fae7a2f3842bff80
Katie Hoffman <ktahoffman@gmail.com>
2014-02-11 16:48:28 -0500

    changes hash to array

--- spotify.rb
+++ spotify.rb
@@ -8,12 +8,14 @@ class Spotify
     @song = JSON.parse(open("http://charts.spotify.com/api/charts/most_shared/" + country + "/latest").first)["tracks"][0]
   end
   def info_hash
-    {song["country"] =>
-      {:url=>song["track_url"]}, 
-      {:title=>song["track_name"]}, 
-      {:artist=>song["artist_name"]},
-      {:album=>song["album_name"]},
-      {:artwork=>song["artwork_url"]}
+    {song["country"] => 
+      [
+        {"url"     => song["track_url"]}, 
+        {"title"   => song["track_name"]}, 
+        {"artist"  => song["artist_name"]},
+        {"album"   => song["album_name"]},
+        {"artwork" => song["artwork_url"]}
+      ]
     }
   end
 end
